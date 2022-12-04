429e8cb29c2c539eeba255c3c2b3e6f79cf84a95
Valepa2911 <megp17102017@mail.ru>
2022-12-04 17:08:18 +0400

    сохран13.

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int number = 0;
+if (numA>=100)
+{ 
+    number = (numA%100)/10;
+    Console.WriteLine(number);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
